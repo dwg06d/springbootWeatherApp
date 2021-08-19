de673bd6731fd559e2efd3f18454def33ce2e98a
David Griffin <dwg06d@gmail.com>
2021-08-19 10:59:15 -0500

    Updated Functionality
    Add the necessary database settings to application.properties.
    Create a model/entity to store zip codes in the database.
    Create a zip code repository.
    Inject the zip code repository into the weather service.
    Any time the getForecast method is called in the weather service, add the zip code to the database.
    Create a weather service method to get the (up to 10) most recent searches.
    In both of the weather controller methods, call the new service method and add the the list of zip codes to the model.
    In both of the weather controller methods, call the new service method and add the the list of zip codes to the model.
    In the html page, add code to display the recent searches in a table.

--- src/main/java/com/tts/weatherapp/service/WeatherService.java
+++ src/main/java/com/tts/weatherapp/service/WeatherService.java
@@ -2,21 +2,36 @@ package com.tts.weatherapp.service;
 
 
 import com.tts.weatherapp.model.Response;
+import com.tts.weatherapp.model.ZipCode;
+import com.tts.weatherapp.repository.ZipCodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WeatherService {
     @Value("${api_key}")
     private String apiKey;
 
+    ZipCodeRepository zipCodeRepository;
+
+    public WeatherService(ZipCodeRepository zipCodeRepository){
+        this.zipCodeRepository = zipCodeRepository;
+    }
+
+
     public Response getForecast(String zipCode) {
         String url = "http://api.openweathermap.org/data/2.5/weather?zip=" +
                 zipCode + "&units=imperial&appid=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
         try {
+            zipCodeRepository.save(new ZipCode(zipCode));
             return restTemplate.getForObject(url, Response.class);
         } catch (HttpClientErrorException ex) {
             Response response = new Response();
@@ -24,4 +39,16 @@ public class WeatherService {
             return response;
         }
     }
+
+//    Method to return the last 10 Search
+
+    public List<ZipCode> getLast10ZipCode(){
+      List<ZipCode> zipCodeList = (List<ZipCode>) zipCodeRepository.findAll();
+        Collections.reverse(zipCodeList);
+      return zipCodeList.stream()
+              .limit(10)
+              .collect(Collectors.toList());
+    }
+
+
 }
